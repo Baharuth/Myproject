bfba13212dbc523f6e8e1aae36f240a1cdfa28ac
Frajer12 <radek.lopa@gmail.com>
2026-01-17 17:11:05 +0100

    Create item.cs

---
+++ Assets/StarterAssets/FirstPersonController/Scripts/item.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public bool isPickedUp = false;
+
+    // Zde mùžeš pozdìji pøidat další vìci, 
+    // napø. typ pøedmìtu (zbraò/jídlo)
+}
