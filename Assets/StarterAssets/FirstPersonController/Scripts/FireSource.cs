86fd421be6a8bfd6f091df156ed47192423dcce0
Frajer12 <radek.lopa@gmail.com>
2026-01-17 19:22:06 +0100

    Update FireSource.cs

--- Assets/StarterAssets/FirstPersonController/Scripts/FireSource.cs
+++ Assets/StarterAssets/FirstPersonController/Scripts/FireSource.cs
@@ -2,28 +2,40 @@ using UnityEngine;
 
 public class FireSource : MonoBehaviour
 {
-    public ParticleSystem fireParticles; // Pøetáhni sem èástice ohnì
-    public float fireHealth = 100f;      // "Životy" ohnì
-    public float extinguishRate = 20f;   // Jak rychle oheò uhasne
+    public ParticleSystem fireParticles;
+    public float fireHealth = 100f;
+    private float currentHealth;
+
+    void Start()
+    {
+        currentHealth = fireHealth;
+    }
+
+    // TATO METODA JE PRO ÈÁSTICE NEJSPOLEHLIVÌJŠÍ
+    private void OnParticleCollision(GameObject other)
+    {
+        // Debug, abychom vidìli, že èástice do nìèeho narazila
+        Debug.Log("<color=cyan>Kolize:</color> Èástice z " + other.name + " zasáhla oheò!");
+
+        ReduceFire(1.0f);
+    }
 
     public void ReduceFire(float amount)
     {
-        fireHealth -= amount;
+        currentHealth -= amount;
+        Debug.Log("<color=orange>Oheò HP:</color> " + currentHealth);
 
-        // Pokud oheò slábne, mùžeme zmenšit emisi èástic (dobrovolné)
-        var emission = fireParticles.emission;
-        emission.rateOverTime = (fireHealth / 100f) * 50f; // Pøíklad zmenšování
-
-        if (fireHealth <= 0)
+        if (fireParticles != null)
         {
-            FinishExtinguishing();
+            var emission = fireParticles.emission;
+            emission.rateOverTime = (currentHealth / fireHealth) * 40f;
         }
-    }
 
-    void FinishExtinguishing()
-    {
-        fireParticles.Stop();
-        Debug.Log("Oheò byl uhašen!");
-        // Volitelnì: Destroy(gameObject, 2f);
+        if (currentHealth <= 0)
+        {
+            Debug.Log("<color=green>Oheò uhašen!</color>");
+            if (fireParticles != null) fireParticles.Stop();
+            gameObject.SetActive(false); // Oheò zmizí úplnì
+        }
     }
 }
